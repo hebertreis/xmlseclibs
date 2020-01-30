caeb91e7c46debd0d8deceadadb5f60ab28cbf73
Hebert Reis <hebertreis@users.noreply.github.com>
2020-01-29 22:28:38 -0300

    Append signature before Element Position
    Append signature beforeElementPosition to work with Twilio Flex SSO

--- src/XMLSecurityDSig.php
+++ src/XMLSecurityDSig.php
@@ -908,9 +908,10 @@ class XMLSecurityDSig
      * @param bool $insertBefore
      * @return DOMNode
      */
-    public function appendSignature($parentNode, $insertBefore = false)
+    public function appendSignature($parentNode, $insertBefore = false, $beforeElementPosition = 0)
     {
         $beforeNode = $insertBefore ? $parentNode->firstChild : null;
+		$beforeNode = $beforeElementPosition > 0 ? $parentNode->childNodes[$beforeElementPosition] : $beforeNode;
         return $this->insertSignature($parentNode, $beforeNode);
     }
 
